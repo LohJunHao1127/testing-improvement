8777d918bc9bb0628564b2d0ca99ebb69d97f2ac
LohJunHao1127 <115975421+LohJunHao1127@users.noreply.github.com>
2023-08-13 06:21:35 +0800

    added comments for fun ig

--- models/records.js
+++ models/records.js
@@ -2,7 +2,32 @@ const pool = require('../database');
 
 // Records Endpoint 1: Insert highscore after checking if it is the highest.
 
+/** 
+  * @param {number} userid - userid of the user that should be inserted
+  * @param {number} gameid - gameid of the game that should be inserted
+  * @param {number} score - score of the game that should be inserted
+  * @returns {Promise<{success: boolean, message: string}>}
+  * @throws {Error} - database errors
+  * @description Inserts a new score for a user and game. If the score is higher than the current highscore, the highscore will be updated.
+  * @example 
+  * // Insert a new score for user with ID 1 and game with ID 1
+  * const result = await records.createNewScore(1, 1, 100);
+  * console.log(result.message); // Expected output: "Score inserted successfully."
+  * console.log(result.success); // Expected output: true
+  *
+  * @example
+  * // Attempt to insert a score that is lower than the existing highscore
+  * const result2 = await records.createNewScore(1, 1, 50);
+  * console.log(result2.message); // Expected output: "Failed to insert score ."
+  * console.log(result2.success); // Expected output: false
+  *
+  * @example
+  * // Attempt to insert a score that is higher than the existing highscore
+  * const result3 = await records.createNewScore(1, 1, 200);
+  * console.log(result3.message); // Expected output: "Score inserted successfully."
+  * console.log(result3.success); // Expected output: true
 
+*/
 
 module.exports.createNewScore = function (userid, gameid, score) {
   return pool
@@ -55,7 +80,35 @@ module.exports.createNewScore = function (userid, gameid, score) {
 };
 
 
-module.exports.createNewScoreTime = function (userid, gameid, score ,time) {
+/** 
+  * @param {number} userid - userid of the user that should be inserted
+  * @param {number} gameid - gameid of the game that should be inserted
+  * @param {number} score - score of the game that should be inserted
+  * @param {number} time - time of the game that should be inserted
+  * @returns {Promise<{success: boolean, message: string}>}
+  * @throws {Error} - database errors
+  * @description Inserts a new score for a user and game alng with the time. If the score is higher than the current highscore, the highscore will be updated.
+  * @example 
+  * // Insert a new score for user with ID 1 and game with ID 1
+  * const result = await records.createNewScore(1, 1, 100 , 100);
+  * console.log(result.message); // Expected output: "Score inserted successfully."
+  * console.log(result.success); // Expected output: true
+  *
+  * @example
+  * // Attempt to insert a score that is lower than the existing highscore
+  * const result2 = await records.createNewScore(1, 1, 50 , 100);
+  * console.log(result2.message); // Expected output: "Failed to insert score ."
+  * console.log(result2.success); // Expected output: false
+  *
+  * @example
+  * // Attempt to insert a score that is higher than the existing highscore
+  * const result3 = await records.createNewScore(1, 1, 200 , 100);
+  * console.log(result3.message); // Expected output: "Score inserted successfully."
+  * console.log(result3.success); // Expected output: true
+
+*/
+
+module.exports.createNewScoreTime = function (userid, gameid, score, time) {
   return pool
     .query(
       "SELECT MAX(highscore) AS maxScore FROM record WHERE userid = ? AND gameid = ?",
@@ -71,7 +124,7 @@ module.exports.createNewScoreTime = function (userid, gameid, score ,time) {
         return pool
           .query(
             "INSERT INTO record (userid, gameid, score, highscore,duration) VALUES (?, ?, ?, ? , ?)",
-            [userid, gameid, score, maxScore , time]
+            [userid, gameid, score, maxScore, time]
           )
           .then(() => {
             return {
@@ -87,7 +140,7 @@ module.exports.createNewScoreTime = function (userid, gameid, score ,time) {
         return pool
           .query(
             "INSERT INTO record (userid, gameid, score, highscore ,duration) VALUES (?, ?, ?, ? ,?) ON DUPLICATE KEY UPDATE score = VALUES(score), highscore = VALUES(highscore)",
-            [userid, gameid, score, score , time]
+            [userid, gameid, score, score, time]
           )
           .then(() => {
             return {
@@ -111,11 +164,27 @@ module.exports.createNewScoreTime = function (userid, gameid, score ,time) {
 
 
 
-// Records Endpoint 2: Get all scores of the user with their userid and gameid
-//changed from ca1
-module.exports.getAllScore = function (userid,gameid) {
+
+/** 
+  * @param {number} userid - userid of the user that is needed to be searched
+  * @param {number} gameid - gameid of the game that is needed to be searched
+  * @returns {Promise<{success: boolean, message: string}>}
+  * @throws {Error} - database errors
+  * @description Searches for all the scores of a user and game .
+  * @example 
+  * // Insert a new score for user with ID 1 and game with ID 1
+  * const result = await records.getAllScore(1, 1);
+  * console.log(result); // Expected output: "records of the user and game ."
+  *
+  * @example
+  * // Attempt to insert a score that is lower than the existing highscore
+  * const result2 = await records.getAllScore(100, 1);
+  * console.log(result2); // Expected output: "[]" (empty array)
+  * 
+*/
+module.exports.getAllScore = function (userid, gameid) {
   return pool
-    .query("SELECT score ,timestamp FROM record WHERE userid = ? AND gameid =? ORDER BY timestamp ASC;", [userid,gameid])
+    .query("SELECT score ,timestamp FROM record WHERE userid = ? AND gameid =? ORDER BY timestamp ASC;", [userid, gameid])
     .then((result) => {
       if (result.length === 0) {
         throw new NotFoundError(`User ${userid} not found`);
@@ -197,7 +266,7 @@ module.exports.getAllHighscore = function (userid, gameid, date) {
     });
 };
 
-module.exports.getGamesPlayedOverTime  = function (userid ) {
+module.exports.getGamesPlayedOverTime = function (userid) {
   return pool
     .query("SELECT DATE(timestamp) AS date, SUM(games_played) AS total_games_played FROM ( SELECT u.username, r.gameid, DATE(timestamp) AS timestamp, COUNT(*) AS games_played FROM record r INNER JOIN user u ON u.userid = r.userid WHERE u.userid = ? GROUP BY u.username, r.gameid, DATE(timestamp) ) AS subquery GROUP BY DATE(timestamp) ORDER BY DATE(timestamp) ASC;", [userid])
     .then((result) => {
